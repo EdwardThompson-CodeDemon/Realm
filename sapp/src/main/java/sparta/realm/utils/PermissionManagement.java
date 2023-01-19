31db7992c8effcd0d8400db3523b5afd0a4feff5
Edward Thompson <thompsonseddy@gmail.com>
2023-01-20 02:54:20 +0300

    changed target sdk and compile sdk to 33
    for permission management

--- sapp/src/main/java/sparta/realm/utils/PermissionManagement.java
+++ sapp/src/main/java/sparta/realm/utils/PermissionManagement.java
@@ -9,6 +9,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.provider.Settings;
 import android.util.Log;
 
@@ -20,6 +21,8 @@ import java.util.List;
 
 import sparta.realm.Realm;
 
+import static android.os.Build.VERSION.SDK_INT;
+
 public class PermissionManagement {
 
     public static final int MULTIPLE_PERMISSIONS = 10;
@@ -29,12 +32,40 @@ public class PermissionManagement {
         try {
             PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_PERMISSIONS);
             if (info.requestedPermissions != null) {
-                checkPermissions(context, info.requestedPermissions);
+                checkAndRequestPermissions(context, info.requestedPermissions);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+
+    }
+
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public static void openOverlaySettings(Activity activity) {
+        final Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:" + activity.getPackageName()));
+        try {
+            activity.startActivityForResult(intent, 6);
+        } catch (ActivityNotFoundException e) {
+            Log.e("Drawers permission :", e.getMessage());
+        }
+    }
+
+
+    public static boolean checkAndRequestAllPermissions(Activity context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_PERMISSIONS);
+            if (info.requestedPermissions != null) {
+                return checkAndRequestPermissions(context, info.requestedPermissions);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
 
     }
 
@@ -52,6 +83,66 @@ public class PermissionManagement {
 
     }
 
+    public static boolean checkAndRequestPermissions(Activity context, String[] permissions) {
+        int result;
+        boolean ok = true;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(context, p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (listPermissionsNeeded.contains(Manifest.permission.MANAGE_EXTERNAL_STORAGE)) {
+
+            if (SDK_INT >= Build.VERSION_CODES.R) {
+                if (Environment.isExternalStorageManager()) {
+                    listPermissionsNeeded.remove(Manifest.permission.MANAGE_EXTERNAL_STORAGE);
+                } else {
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                    Uri uri = Uri.fromParts("package", context.getPackageName(), null);
+                    intent.setData(uri);
+                    context.startActivity(intent);
+                }
+            } else {
+                //below android 11=======
+                listPermissionsNeeded.remove(Manifest.permission.MANAGE_EXTERNAL_STORAGE);
+            }
+        }
+        if (listPermissionsNeeded.contains(Manifest.permission.SYSTEM_ALERT_WINDOW)) {
+            if (!Settings.canDrawOverlays(context)) {
+                openOverlaySettings(context);
+                ok = false;
+            } else {
+                listPermissionsNeeded.remove(Manifest.permission.SYSTEM_ALERT_WINDOW);
+
+
+            }
+
+        }
+        if (listPermissionsNeeded.contains(Manifest.permission.REQUEST_INSTALL_PACKAGES)) {
+            listPermissionsNeeded.remove(Manifest.permission.REQUEST_INSTALL_PACKAGES);
+
+        }
+
+        if (listPermissionsNeeded.isEmpty() || (listPermissionsNeeded.size() == 1 & listPermissionsNeeded.get(0).equalsIgnoreCase(Manifest.permission.ACCESS_FINE_LOCATION))) {
+
+        } else {
+            ActivityCompat.requestPermissions(context, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), MULTIPLE_PERMISSIONS);
+            ok = false;
+        }
+
+
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.SYSTEM_ALERT_WINDOW) != PackageManager.PERMISSION_GRANTED) {
+
+
+            //  openOverlaySettings();
+        }
+
+
+        return ok;
+    }
+
     public static boolean checkPermissions(Activity context, String[] permissions) {
         int result;
         boolean ok = true;
@@ -62,39 +153,36 @@ public class PermissionManagement {
                 listPermissionsNeeded.add(p);
             }
         }
+        if (listPermissionsNeeded.contains(Manifest.permission.MANAGE_EXTERNAL_STORAGE)) {
+
+            if (SDK_INT >= Build.VERSION_CODES.R) {
+                if (Environment.isExternalStorageManager()) {
+                    listPermissionsNeeded.remove(Manifest.permission.MANAGE_EXTERNAL_STORAGE);
+                }
+            } else {
+                listPermissionsNeeded.remove(Manifest.permission.MANAGE_EXTERNAL_STORAGE);
+            }
+        }
+        if (listPermissionsNeeded.contains(Manifest.permission.SYSTEM_ALERT_WINDOW)) {
+            if (!Settings.canDrawOverlays(context)) {
+                ok = false;
+            } else {
+                listPermissionsNeeded.remove(Manifest.permission.SYSTEM_ALERT_WINDOW);
+            }
+        }
+        if (listPermissionsNeeded.contains(Manifest.permission.REQUEST_INSTALL_PACKAGES)) {
+            listPermissionsNeeded.remove(Manifest.permission.REQUEST_INSTALL_PACKAGES);
+        }
 
         if (listPermissionsNeeded.isEmpty() || (listPermissionsNeeded.size() == 1 & listPermissionsNeeded.get(0).equalsIgnoreCase(Manifest.permission.ACCESS_FINE_LOCATION))) {
 
         } else {
-            ActivityCompat.requestPermissions(context, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), MULTIPLE_PERMISSIONS);
             ok = false;
         }
-//        if (!listPermissionsNeeded.isEmpty() && (listPermissionsNeeded.size()!=1&listPermissionsNeeded.get(0).equalsIgnoreCase(Manifest.permission.ACCESS_FINE_LOCATION))) {
-//
-//        }
 
-
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.SYSTEM_ALERT_WINDOW) != PackageManager.PERMISSION_GRANTED) {
-
-
-            //  openOverlaySettings();
-        }
-        if (!Settings.canDrawOverlays(context)) {
-            openOverlaySettings(context);
-            ok = false;
-        }
 
         return ok;
     }
 
-    @TargetApi(Build.VERSION_CODES.M)
-    public static void openOverlaySettings(Activity activity) {
-        final Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                Uri.parse("package:" + activity.getPackageName()));
-        try {
-            activity.startActivityForResult(intent, 6);
-        } catch (ActivityNotFoundException e) {
-            Log.e("Drawers permission :", e.getMessage());
-        }
-    }
+
 }
